d1288e2c792759779a8554fb5e9607399d8a0e4a
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-09-01 19:37:27 +0300

    Адаптація Nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import "normalize.css";
 import Layout from "./Layout/Layout";
-import RestrictedRoute from "./RestrictedRoute/RestrictedRoute";
-import PrivateRoute from "./PrivateRoute/PrivateRoute";
 import { refreshUser } from "../redux/auth/operations";
 import { selectIsRefreshing } from "../redux/auth/selectors";
 import css from "./App.module.css";
 
 const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
 const ContactsPage = lazy(() => import("../pages/ContactsPage/ContactsPage"));
-const RegistrationPage = lazy(() =>
-  import("../pages/RegistrationPage/RegistrationPage")
-);
-const LogInPage = lazy(() => import("../pages/LogInPage/LogInPage"));
 const NotFoundPage = lazy(() => import("../pages/NotFoundPage/NotFoundPage"));
 
 function App() {
@@ -33,31 +27,7 @@ function App() {
       <Suspense fallback={<div className={css.apploading}>Loading...</div>}>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route
-            path="/contacts"
-            element={
-              <PrivateRoute component={<ContactsPage />} redirectTo="/login" />
-            }
-          />
-
-          <Route
-            path="/register"
-            element={
-              <RestrictedRoute
-                component={<RegistrationPage />}
-                redirectTo="/contacts"
-              />
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute
-                component={<LogInPage />}
-                redirectTo="/contacts"
-              />
-            }
-          />
+          <Route path="/contacts" element={<ContactsPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,25 +1,31 @@
-import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import clsx from "clsx";
-import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import css from "./Navigation.module.css";
 const navItem = ({ isActive }) => {
   return clsx(css.link, isActive && css.active);
 };
 
 function Navigation() {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-
   return (
     <nav className={css.navigation}>
       <NavLink to="/" className={navItem}>
         Home
       </NavLink>
-      {isLoggedIn && (
-        <NavLink to="/contacts" className={navItem}>
-          Contacts
-        </NavLink>
-      )}
+      <NavLink to="/products" className={navItem}>
+        Products
+      </NavLink>
+      <NavLink to="/team" className={navItem}>
+        Team
+      </NavLink>
+      <NavLink to="/faq" className={navItem}>
+        FAQ
+      </NavLink>
+      <NavLink to="/reviews" className={navItem}>
+        Reviews
+      </NavLink>
+      <NavLink to="/contacts" className={navItem}>
+        Contacts
+      </NavLink>
     </nav>
   );
 }
